ad8b2d68991ddb2550d715a85d1e57b3ba1a7daf
shahriarAb <sm.abeed007@gmail.com>
2021-11-09 01:06:31 +0600

    server added

---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const admin = require("firebase-admin");
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 5500;
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8cjcg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+    next();
+}
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('doctors_portal');
+        const appointmentCollection = database.collection('appointments');
+        const usersCollection = database.collection('users');
+
+        //GET api
+        app.get('/appointments', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            const query = { email: email, date: date }
+            const cursor = appointmentCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+        //GET api to get admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        // POST api for appointments
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentCollection.insertOne(appointment);
+            res.json(result);
+        })
+
+        // POST api for new regiterd user
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        })
+
+        //PUT api for handle google sign in user using upsert
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+
+        //PUT api to make someone admin with email
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'You have no access to make someone Admin!' });
+            }
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Doctors Portal Server is running now!');
+});
+
+app.listen(port, () => {
+    console.log(`doctors-portal-server listening at http://localhost:${port}`);
+});
